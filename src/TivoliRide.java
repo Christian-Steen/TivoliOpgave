93cfb8c8b90cf92a075a374fc0def1450f7fd38c
Christian-Steen <steen.christian@icloud.com>
2026-02-05 10:00:07 +0100

    Første Ændring

--- src/TivoliRide.java
+++ src/TivoliRide.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class TivoliRide {
+    public static void main(String[] args) {
+        System.out.println("How tall are you?(in cm)");
+
+        // højde
+        int tall;
+
+        // Scanner
+        Scanner st = new Scanner(System.in);
+        tall = st.nextInt();
+        if (tall >= 170) {
+            System.out.println("You are tall enough for the ride");
+        } else {
+            System.out.println("You dont meet the height requirement");
+        }
+    }
 }
